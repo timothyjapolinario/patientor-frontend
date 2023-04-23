4b9ccda892bc58a2aece6f112c30226605175fec
timothyjapolinario <timothyjapolinario@gmail.com>
2023-04-23 12:50:47 +0800

    Add starter files

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "../node_modules/react-router-dom/dist/index";
+
+import Patients from "./pages/patients";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Patients />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Patients.tsx
@@ -0,0 +1,9 @@
+const Patients = () => {
+  return (
+    <>
+      <h1>Patient Page</h1>
+    </>
+  );
+};
+
+export default Patients;
