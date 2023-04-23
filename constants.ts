f49f9f805791a4e4affb2348924b52af6aa76296
timothyjapolinario <timothyjapolinario@gmail.com>
2023-04-23 18:21:35 +0800

    Update app for deployment

--- constants.ts
+++ constants.ts
@@ -1,1 +1,3 @@
 export const apiBaseUrl = "http://localhost:3001/api";
+export const apiDeployedBaseUrl =
+  "https://patientor-backend-7me0.onrender.com/api";
--- src/utils/patientService.ts
+++ src/utils/patientService.ts
@@ -1,9 +1,14 @@
 import axios from "../../node_modules/axios/index";
 import { Patient } from "../types";
-import { apiBaseUrl } from "../../constants";
+import { apiDeployedBaseUrl, apiBaseUrl } from "../../constants";
+
+const MODE = process.env.NODE_ENV;
+const baseUrl = MODE === "production" ? apiDeployedBaseUrl : apiBaseUrl;
 
 const getAll = async () => {
-  const { data } = await axios.get<Patient[]>(`${apiBaseUrl}/patients`);
+  console.log(process.env.NODE_ENV);
+  const { data } = await axios.get<Patient[]>(`${baseUrl}/patients`);
+  console.log(data);
   return data;
 };
 
